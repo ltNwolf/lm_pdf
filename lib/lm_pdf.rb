26b8da415415437733019efc9e65babaf7b2ecb1
Lt Neowolf <ltneowolf@gmail.com>
2026-06-07 22:05:41 -0500

    Initial commit lm_pdf gem

---
+++ lib/lm_pdf.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "lm_pdf/version"
+
+module LmPdf
+  def self.hello
+    "AAAAAAAAAAAAAAAAA"
+  end
+end
---
+++ spec/lm_pdf_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LmPdf do
+  it "has a version number" do
+    expect(LmPdf::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
